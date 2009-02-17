17ae1ad1ae8a14191a5216b48dd41717a5a78fda
Stephen Paul Weber <singpolyma@singpolyma.net>
2009-02-17 11:25:34 -0500

    More manual revert. Whatever.

--- bitconvert.c
+++ bitconvert.c
@@ -335,7 +335,7 @@ int bc_decode_fields(struct bc_decoded* d)
 	int err;
 	int rc;
 	int rv;
-   char name[FORMAT_LEN]; /* For skipping lines... */
+	char name[FORMAT_LEN];
 	FILE* formats;
 
 	formats = fopen("formats", "r");
@@ -347,12 +347,15 @@ int bc_decode_fields(struct bc_decoded* d)
 	rv = BCERR_NO_MATCHING_FORMAT;
 
 	/* initialize the name and fields list */
+	d->name[0] = '\0';
 	d->field_names[0][0] = '\0';
 
-	while (fgets(d->name, FORMAT_LEN, formats)) {
+	while (fgets(name, FORMAT_LEN, formats)) {
 		/* TODO: check lengths before strcpy (in general, but here esp.)
 		 */
-		d->name[strlen(d->name) - 1] = '\0'; /* remove '\n' */
+		name[strlen(name) - 1] = '\0'; /* remove '\n' */
+		strcpy(d->name, name);
+
 
 		err = bc_decode_track_fields(d->t1, d->t1_encoding, BC_TRACK_1,
 			formats, d);
@@ -412,7 +415,7 @@ int bc_decode_fields(struct bc_decoded* d)
 		while (fgets(name, FORMAT_LEN, formats) && name[0] != '\n');
 
 		/* empty the name and fields list */
-		d->name = "";
+		d->name[0] = '\0';
 		d->field_names[0][0] = '\0';
 	}
 
